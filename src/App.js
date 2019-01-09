67abf0fd96d9566733bfc6b80df835c8e9dff13e
lubin <lu.bin.lu.bin@gmail.com>
2019-01-09 00:05:43 -0500

    grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
-import { Button, Container } from "reactstrap";
+import { Button, Container, Row, Col } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
-      <Container className="custom-class">
-        <div class="bd-example-container-header" />
-        <div class="bd-example-container-sidebar" />
-        <div class="bd-example-container-body" />
+      <Container>
+        <Row>
+          <Col sm={true}>One of three columns</Col>
+          <Col sm={true}>One of three columns</Col>
+          <Col sm={true}>One of three columns</Col>
+        </Row>
       </Container>
     );
   }
