839e75c7563ae882609aa481d94a0ff31c34fd46
Bin Lu <lu.bin.lu.bin@gmail.com>
2019-01-09 16:51:13 -0500

    grid continue

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import { Button, Container, Row, Col } from "reactstrap";
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>
+      <Container className="outline-r">
+        <Row className="outline-g">
+          <Col sm={true} className="outline-b">One of three columns</Col>
           <Col sm={true}>One of three columns</Col>
-          <Col sm={true}>One of three columns</Col>
-          <Col sm={true}>One of three columns</Col>
+          <Col sm={5}>One of three columns</Col>
+          <Col lg={true}>One of three columns</Col>
         </Row>
+        <Row className="outline-g">
+          <Col md={true} width={50} className="outline-b">One of three columns</Col>
+          <Col md={true}>One of three columns</Col>
+          <Col md={true}>One of three columns</Col>
+        </Row>        
+        <div>not a row</div>
       </Container>
     );
   }
