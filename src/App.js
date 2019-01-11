c259993ae83187b555686ba4a7731d2552411e73
Bin Lu <lu.bin.lu.bin@gmail.com>
2019-01-11 18:35:06 -0500

    responsive typography

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ import { Button, Container, Row, Col } from "reactstrap";
 class App extends Component {
   render() {
     return (
-      <div className="media">
-        <img className="mr-3" src={myimage} alt="Generic placeholder" />
-        <div className="media-body">
-          <h5 className="mt-0">Media heading</h5>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-          scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-          vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-          vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </div>
+      <div>
+        <ul class="list-unstyled">
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Consectetur adipiscing elit</li>
+          <li>Integer molestie lorem at massa</li>
+          <li>Facilisis in pretium nisl aliquet</li>
+          <li>
+            Nulla volutpat aliquam velit
+            <ul class="list-unstyled">
+              <li>Phasellus iaculis neque</li>
+              <li>Purus sodales ultricies</li>
+              <li>Vestibulum laoreet porttitor sem</li>
+              <li>Ac tristique libero volutpat at</li>
+            </ul>
+          </li>
+          <li>Faucibus porta lacus fringilla vel</li>
+          <li>Aenean sit amet erat nunc</li>
+          <li>Eget porttitor lorem</li>
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './custom.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
